3e18448108f949a82f0574d4f213819e7ab9f9bb
baekchangjoon <changjoon.baek@icloud.com>
2025-01-27 21:33:52 +0900

    Split cucumber from src/test

--- src/cucumber/java/com/example/runner/RunCucumberIT.java
+++ src/cucumber/java/com/example/runner/RunCucumberIT.java
@@ -7,11 +7,11 @@ import io.cucumber.junit.CucumberOptions;
 // JUnit 4 기준 예시
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",  // Feature 파일 경로
+        features = "src/cucumber/resources/features",  // Feature 파일 경로
         glue = {"com.example.steps"},    // StepDefinition 패키지 경로
         plugin = {"pretty", "json:target/cucumber.json", "html:target/Cucumber.html"},
         monochrome = true
 )
-public class CucumberTest {
+public class RunCucumberIT {
     // 별도 코드 없이 어노테이션 설정으로 실행
 }
